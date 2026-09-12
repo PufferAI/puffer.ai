1c178b3dad58818c35cb1a00f8dc216550cb37e3
Joseph Suarez <jsuarez@puffer.ai>
2026-09-12 23:22:41 +0000

    favicon

--- docs/header.js
+++ docs/header.js
@@ -1,6 +1,17 @@
 (function () {
     var script = document.currentScript;
     var base = script && script.src ? script.src.replace(/header\.js(?:\?.*)?$/, '') : '';
+    function addIcon(rel, href, type, sizes) {
+        var l = document.createElement('link');
+        l.rel = rel;
+        l.href = href;
+        if (type) l.type = type;
+        if (sizes) l.setAttribute('sizes', sizes);
+        document.head.appendChild(l);
+    }
+    addIcon('icon', base + 'favicon.ico');
+    addIcon('icon', base + 'favicon.png', 'image/png', '32x32');
+    addIcon('apple-touch-icon', base + 'apple-touch-icon.png');
     fetch(base + 'header.html')
         .then(function (response) { return response.text(); })
         .then(function (html) {
